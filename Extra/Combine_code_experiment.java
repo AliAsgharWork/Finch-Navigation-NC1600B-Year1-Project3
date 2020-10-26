c80ea69a03054983082ba790da4cf62f8a61fa50
Ali Asghar <aliasgharwork@gmail.com>
2020-10-26 12:21:40 +0000

    Combine Code Experiment Addition
    Add all the code of our project together as a single application

---
+++ Extra/Combine_code_experiment.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Combine_code_experiment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			MainClass.main(args);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
